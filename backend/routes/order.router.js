80ec58f7b6124fa26177fd4ad64b0db921298d80
Suvin Tennakoon <71589486+Suvin-Tennakoon@users.noreply.github.com>
2022-10-08 23:45:13 +0530

    Add order routes

--- backend/routes/order.router.js
+++ backend/routes/order.router.js
@@ -1,7 +1,27 @@
-const express = require('express');
+const express = require("express");
+const {
+  addNewOrder,
+  getAllCakeOrders,
+  getCakesforSeller,
+  getCakesforCustomer,
+  getOrderData,
+  getOrderProgress,
+  updateOrder,
+  setAcceptedOrder,
+  setRejectOrder,
+} = require("../controllers/Order/order.controller");
 const orderRoutes = express.Router();
 
 //code
 
+orderRoutes.post("/addCakeOrder", addNewOrder);
+orderRoutes.get("/getAllOrders", getAllCakeOrders);
+orderRoutes.get("/getCakeforShop/:seller", getCakesforSeller);
+orderRoutes.get("/getCakeforCustomer/:customer", getCakesforCustomer);
+orderRoutes.get("/getOrderData/:id", getOrderData);
+orderRoutes.get("/getOrderProgress/:id", getOrderProgress);
+orderRoutes.put("/updateOrder/:id", updateOrder);
+orderRoutes.put("/setAcceptedOrder/:id", setAcceptedOrder);
+orderRoutes.put("/setRejectedOrder/:id", setRejectOrder);
 
 module.exports = orderRoutes;
