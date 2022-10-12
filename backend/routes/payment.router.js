1565d0d1cb2f7729e7e1de3c4e2c0278fddc7657
VisharaPrabuddhi <86484671+VisharaPrabuddhi@users.noreply.github.com>
2022-10-12 20:03:10 +0530

    z

--- backend/routes/payment.router.js
+++ backend/routes/payment.router.js
@@ -2,6 +2,8 @@ const express = require('express');
 const paymentRoutes = express.Router();
 const { addPayment, getPayment, getPaymentById, updatePayment} = require('../controllers/Payment/Payment');
 const { addcouponcode, getCouponCode, getCouponCodeById, updateCouponCode} = require('../controllers/Payment/CouponCode');
+// const stripe = require("stripe")("sk_test_51Ls317GiNApYk7vMcCFqXQgZUH1ENU14Q11MOYgCR8f9LFA2maXRUCiPUrR5GMQ18GpQp9KhLg3NWUdE9B24Nn4I001wFHOGw8")
+// const {v4 : uuidv4 } = require('uuid');
 
 paymentRoutes.post('/', addPayment);
 paymentRoutes.get('/get', getPayment);
@@ -11,5 +13,6 @@ paymentRoutes.post('/coupon', addcouponcode);
 paymentRoutes.get('/get/coupon', getCouponCode);
 paymentRoutes.get('/get/coupon/:id', getCouponCodeById);
 paymentRoutes.put('/update/coupon/:id', updateCouponCode);
+
 
 module.exports = paymentRoutes;
