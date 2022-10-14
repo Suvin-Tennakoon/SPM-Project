36bc3d9c426fae4a6d05dca85185ee643b7ceb8f
VisharaPrabuddhi <86484671+VisharaPrabuddhi@users.noreply.github.com>
2022-10-15 02:34:52 +0530

    rating backend

--- backend/routes/payment.router.js
+++ backend/routes/payment.router.js
@@ -14,7 +14,7 @@ const {
   deleteCouponCode,
   getCouponDiscount,
 } = require("../controllers/Payment/CouponCode");
-const { rate } = require("../controllers/Payment/Rate");
+const { rate, addRate } = require("../controllers/Payment/Rate");
 // const stripe = require("stripe")("sk_test_51Ls317GiNApYk7vMcCFqXQgZUH1ENU14Q11MOYgCR8f9LFA2maXRUCiPUrR5GMQ18GpQp9KhLg3NWUdE9B24Nn4I001wFHOGw8")
 // const {v4 : uuidv4 } = require('uuid');
 
@@ -30,5 +30,6 @@ paymentRoutes.put("/update/coupon/:id", updateCouponCode);
 paymentRoutes.delete("/delete/coupon/:id", deleteCouponCode);
 paymentRoutes.post("/getCouponDiscount", getCouponDiscount);
 paymentRoutes.post("/rate", rate);
+paymentRoutes.post('/addrate/:number', addRate);
 
 module.exports = paymentRoutes;
