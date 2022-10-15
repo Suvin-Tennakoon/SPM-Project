afd137e164a6dc56202e1adc402af85304fb7f16
VisharaPrabuddhi <86484671+VisharaPrabuddhi@users.noreply.github.com>
2022-10-15 22:05:49 +0530

    dev vish admin

--- frontend/src/components/AdminHandling/Home.js
+++ frontend/src/components/AdminHandling/Home.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div style={{ marginTop: "120px" }}>
+    <div style={{ marginTop: "200px"}}>
       <center>
-        <a className='btn btn-warning btn-lg' href='/allcus'>See all users</a>
+        <a className='btn btn-primary btn-lg col-md-9' href='/viewcoupons' style={{}}>View Coupons</a>
         &nbsp;
-        <a className='btn btn-primary btn-lg' href='/allrev'>See all reviews</a>
+        <br></br><a className='btn btn-warning btn-lg col-md-9' href='/allrev'>View Reviews</a>
       </center>
     </div>
   )
